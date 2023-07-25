12b134026d5578ea91b3fd6f876ecee13da798a5
Doodlinka <oleshko.darya@gmail.com>
2023-07-25 13:06:20 +0300

    made he level loading system in a stupid way
    R and T are temporary keys, i intend to create proper signals/actions later. Also, don't do partial resets every frame, it will crash for some reason

--- scripts/LevelManager.gd
+++ scripts/LevelManager.gd
@@ -3,30 +3,38 @@ extends Node2D
 var level_root: Node
 var reset_root: Node
 
+@export_file var first_level_path: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_level(first_level_path)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Input.is_physical_key_pressed(KEY_R):
+		reset()
+	if Input.is_physical_key_pressed(KEY_T):
+		true_reload()
 
-func load(path: String) -> void:
+
+func load_level(path: String) -> void:
 	level_root = ResourceLoader.load(path).instantiate()
-	reset_root = get_node("." + level_root.name + "/RESETABLE")
 	self.add_child(level_root)
-	
+	reset_root = get_node("./" + level_root.name + "/RESETABLE")
+
 
 func true_reload() -> void:
-	level_root = recreate(level_root)
-	self.add_child(level_root)
+	level_root.queue_free()
+	load_level(level_root.scene_file_path)
+
 
 func reset() -> void:
-	reset_root = recreate(reset_root)
+	if not is_instance_valid(reset_root):
+		return
+		
+	reset_root.queue_free()
+	var temp = ResourceLoader.load(level_root.scene_file_path).instantiate()
+	reset_root = temp.get_node("./RESETABLE")
+	temp.remove_child(reset_root)
 	level_root.add_child(reset_root)
-	
-func recreate(node: Node) -> Node:
-	var new = node.duplicate()
-	node.queue_free()
-	return new
+	temp.queue_free()
