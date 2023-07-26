fb87d7f172ee121fe6299786a262e9acf7c812dc
Doodlinka <oleshko.darya@gmail.com>
2023-07-26 13:44:04 +0300

    jump tweaks, moved player file

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,21 +1,25 @@
 extends CharacterBody2D
 
 
-const JUMP_HEIGHT = 3*16
+const MAX_JUMP_HEIGHT = 4*16
+const MIN_JUMP_HEIGHT = 16
 const JUMP_RISE_TIME = 0.35
-const JUMP_FALL_TIME = 0.35
-const COYOTE_TIME = 0.1
-const JUMP_BUFFER_TIME = 0.1
-const MAX_FALL_SPEED = 640
+const JUMP_FALL_TIME = 0.25
+const COYOTE_TIME = 0.15
+const JUMP_BUFFER_TIME = 0.15
+const MAX_FALL_SPEED = 400
+const JUMP_HORIZ_VEL_MULT = 1.2
+
 const MAX_RUN_SPEED = 180
 const RUN_START_TIME = 0.4
 const RUN_STOP_TIME = 0.2
 
-@onready var jump_velocity : float = -((2.0 * JUMP_HEIGHT) / JUMP_RISE_TIME)
-@onready var jump_gravity : float = ((2.0 * JUMP_HEIGHT) / (JUMP_RISE_TIME * JUMP_RISE_TIME))
-@onready var fall_gravity : float = ((2.0 * JUMP_HEIGHT) / (JUMP_FALL_TIME * JUMP_FALL_TIME))
-@onready var run_accel : float = ((2.0 * MAX_RUN_SPEED) / (RUN_START_TIME * RUN_START_TIME))
-@onready var run_decel : float = ((2.0 * MAX_RUN_SPEED) / (RUN_STOP_TIME * RUN_STOP_TIME))
+const JUMP_VELOCITY : float = -((2.0 * MAX_JUMP_HEIGHT) / JUMP_RISE_TIME)
+const VARJUMP_VELOCITY_CLAMP = -((2.0 * MIN_JUMP_HEIGHT) / JUMP_RISE_TIME)
+const JUMP_GRAVITY : float = ((2.0 * MAX_JUMP_HEIGHT) / (JUMP_RISE_TIME * JUMP_RISE_TIME))
+const FALL_GRAVITY : float = ((2.0 * MAX_JUMP_HEIGHT) / (JUMP_FALL_TIME * JUMP_FALL_TIME))
+const RUN_ACCEL : float = ((2.0 * MAX_RUN_SPEED) / (RUN_START_TIME * RUN_START_TIME))
+const RUN_DECEL : float = ((2.0 * MAX_RUN_SPEED) / (RUN_STOP_TIME * RUN_STOP_TIME))
 
 var coyote = COYOTE_TIME
 var jump_buffer = 0.0
@@ -26,9 +30,12 @@ var previous_dir = 0
 func _physics_process(delta: float) -> void:
 	update_timers(delta)
 	
-	# switch gravity
+	# switch gravity (if two gravities are in use)
 	if velocity.y >= 0 or Input.is_action_just_released("jump"):
 		jumping = false
+	# variable jump
+	if velocity.y < VARJUMP_VELOCITY_CLAMP and Input.is_action_just_released("jump"):
+		velocity.y = VARJUMP_VELOCITY_CLAMP
 	
 	# fall
 	velocity.y += get_gravity() * delta
@@ -45,20 +52,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var direction := get_horiz_direction()
 	if direction:
-		velocity.x = move_toward(velocity.x, MAX_RUN_SPEED*direction, run_accel*delta)
+		velocity.x = move_toward(velocity.x, MAX_RUN_SPEED*direction, RUN_ACCEL*delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, run_decel*delta)
+		velocity.x = move_toward(velocity.x, 0, RUN_DECEL*delta)
 
 	move_and_slide()
 
 
 func get_gravity() -> float:
-	return jump_gravity if jumping else fall_gravity
+	return JUMP_GRAVITY if jumping else FALL_GRAVITY
 
 
 func jump():
 	jumping = true
-	velocity.y = jump_velocity
+	velocity.y = JUMP_VELOCITY
+	velocity.x *= JUMP_HORIZ_VEL_MULT
 	coyote = 0
 	jump_buffer = 0
 
