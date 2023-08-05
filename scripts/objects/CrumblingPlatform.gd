7f3c72802c89acb0a83c488a310405ea97383b76
Doodlinka <oleshko.darya@gmail.com>
2023-08-05 16:04:49 +0300

    crumble niose

--- scripts/objects/CrumblingPlatform.gd
+++ scripts/objects/CrumblingPlatform.gd
@@ -5,6 +5,7 @@ extends StaticBody2D
 @onready var collision_shape := $CollisionShape2D
 @onready var area := $Area2D
 @onready var sprite := $NinePatchRect
+@onready var audio := $AudioStreamPlayer
 var image1 := preload("res://sprites/objects/crumble1.png")
 var image2 := preload("res://sprites/objects/crumble2.png")
 
@@ -22,12 +23,14 @@ func _ready() -> void:
 func start_timer(_area) -> void:
 	sprite.texture = image2
 	timer.start()
+	audio.play(0)
 
 
 func on_save_state() -> void:
 	save_data.time = timer.time_left
 	save_data.stopped = timer.is_stopped()
 	save_data.crumbled = crumbled
+	save_data.audio_time = audio.get_playback_position()
 	
 	
 func on_load_state() -> void:
@@ -39,11 +42,14 @@ func on_load_state() -> void:
 			if area.has_overlapping_areas():
 				sprite.texture = image2
 				timer.start(save_data.time)
+				audio.play(save_data.audio_time)
 			else:
 				sprite.texture = image1
 				timer.stop()
+				audio.stop()
 	else:
 		timer.start(save_data.time)
+		audio.play(save_data.audio_time)
 
 
 func set_disabled(value := true) -> void:
